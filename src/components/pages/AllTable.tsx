e0de914eb39b94663a64784099c31137ca3e2591
stopminbeom <mseo@student.42seoul.kr>
2023-06-22 05:04:43 +0900

    Table add

--- src/components/pages/AllTable.tsx
+++ src/components/pages/AllTable.tsx
@@ -2,10 +2,10 @@ import {
 	Table,
 	Thead,
 	Tbody,
-	Tfoot,
 	Tr,
 	Th,
 	Td,
+	Checkbox,
 	TableCaption,
 	TableContainer,
   } from '@chakra-ui/react'
@@ -13,40 +13,53 @@ import {
 export default function AllTable() {
 	return (
 		<TableContainer>
-  <Table variant='striped' colorScheme='teal'>
-    <TableCaption>Imperial to metric conversion factors</TableCaption>
-    <Thead>
-      <Tr>
-        <Th>To convert</Th>
-        <Th>into</Th>
-        <Th isNumeric>multiply by</Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      <Tr>
-        <Td>inches</Td>
-        <Td>millimetres (mm)</Td>
-        <Td isNumeric>25.4</Td>
-      </Tr>
-      <Tr>
-        <Td>feet</Td>
-        <Td>centimetres (cm)</Td>
-        <Td isNumeric>30.48</Td>
-      </Tr>
-      <Tr>
-        <Td>yards</Td>
-        <Td>metres (m)</Td>
-        <Td isNumeric>0.91444</Td>
-      </Tr>
-    </Tbody>
-    <Tfoot>
-      <Tr>
-        <Th>To convert</Th>
-        <Th>into</Th>
-        <Th isNumeric>multiply by</Th>
-      </Tr>
-    </Tfoot>
-  </Table>
-</TableContainer>
+		<Table variant='simple' colorScheme='bule'>
+			<TableCaption>무적의 공격대 레이드 리스트</TableCaption>
+			<Thead>
+			<Tr>
+				<Th>오너</Th>
+				<Th>캐릭터</Th>
+				<Th>발탄</Th>
+				<Th>비아키스</Th>
+				<Th>쿠크세이튼</Th>
+				<Th>아브렐슈드</Th>
+				<Th>하브렐슈드</Th>
+				<Th>카양겔</Th>
+			</Tr>
+			</Thead>
+			<Tbody>
+			<Tr>
+				<Td>할배</Td>
+				<Td>건슬링어</Td>
+				<Td><Checkbox></Checkbox></Td>
+				<Td><Checkbox></Checkbox></Td>
+				<Td><Checkbox></Checkbox></Td>
+				<Td><Checkbox></Checkbox></Td>
+				<Td><Checkbox></Checkbox></Td>
+				<Td><Checkbox></Checkbox></Td>
+			</Tr>
+			<Tr>
+				<Td>할배</Td>
+				<Td>스카우터</Td>
+				<Td><Checkbox></Checkbox></Td>
+				<Td><Checkbox></Checkbox></Td>
+				<Td><Checkbox></Checkbox></Td>
+				<Td><Checkbox></Checkbox></Td>
+				<Td><Checkbox></Checkbox></Td>
+				<Td><Checkbox></Checkbox></Td>
+			</Tr>
+			<Tr>
+			<Td>할배</Td>
+				<Td>슬레이어</Td>
+				<Td><Checkbox></Checkbox></Td>
+				<Td><Checkbox></Checkbox></Td>
+				<Td><Checkbox></Checkbox></Td>
+				<Td><Checkbox></Checkbox></Td>
+				<Td><Checkbox></Checkbox></Td>
+				<Td><Checkbox></Checkbox></Td>
+			</Tr>
+			</Tbody>
+		</Table>
+		</TableContainer>
 	);
 }
